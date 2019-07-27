85096159a876e8719f5555f80a507ff01c16fd75
Nelio Alves <acenelio@gmail.com>
2019-07-27 16:49:52 -0300

    H2 database

--- src/main/java/com/educandoweb/workshop/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/workshop/resources/CategoryResource.java
@@ -1,9 +1,8 @@
 package com.educandoweb.workshop.resources;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,25 +10,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.workshop.entities.Category;
+import com.educandoweb.workshop.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
-	private Map<Long, Category> map = Map.of(
-		1L, new Category(1L, "Electronics"),
-		2L, new Category(2L, "Books")
-	);
+	@Autowired
+	private CategoryService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<Category>> findAll() {
-		var list = new ArrayList<Category>(map.values());
+		var list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Category> findById(@PathVariable Long id) {
-		var obj = map.get(id);
+		var obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
