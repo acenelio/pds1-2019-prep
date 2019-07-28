77d660be3d2a437b45c3659c4717ed3ced3835ea
Nelio Alves <acenelio@gmail.com>
2019-07-28 06:25:01 -0300

    Product entity and repository

--- src/main/java/com/educandoweb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.workshop.entities.Category;
+import com.educandoweb.workshop.entities.Product;
 import com.educandoweb.workshop.repositories.CategoryRepository;
+import com.educandoweb.workshop.repositories.ProductRepository;
 
 @Configuration
 @Profile("test")
@@ -17,6 +19,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -24,6 +29,13 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "", cat2);
+		Product p2 = new Product(null, "Notebook", "Nulla eu imperdiet purus. Maecenas sem ante.", 890.0, "", cat3);
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis. ", 1250.0, "", cat3);
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "", cat3);
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "", cat2);
+				
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
