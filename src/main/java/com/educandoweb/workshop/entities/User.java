2bc25e60dc201fe3455507531a7343e0a30c4e4a
Nelio Alves <acenelio@gmail.com>
2019-07-28 10:59:35 -0300

    User-order one to many association

--- src/main/java/com/educandoweb/workshop/entities/User.java
+++ src/main/java/com/educandoweb/workshop/entities/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.workshop.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -25,6 +28,10 @@ public class User implements Serializable {
 	@JsonIgnore
 	private String password;
 
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -77,6 +84,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
