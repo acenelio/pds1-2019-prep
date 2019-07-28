2b8ad110697420aaae602b69e1ff5be7921e8e1a
Nelio Alves <acenelio@gmail.com>
2019-07-28 06:39:52 -0300

    One to many association

--- src/main/java/com/educandoweb/workshop/entities/Category.java
+++ src/main/java/com/educandoweb/workshop/entities/Category.java
@@ -1,11 +1,16 @@
 package com.educandoweb.workshop.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Category implements Serializable {
@@ -15,6 +20,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "category")
+	private List<Product> products = new ArrayList<>();
 	
 	public Category() {
 	}
@@ -41,6 +50,20 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public List<Product> getProducts() {
+		List<Product> list = new ArrayList<>();
+		list.addAll(products);
+		return list;
+	}
+
+	public void addProduct(Product obj) {
+		products.add(obj);
+	}
+	
+	public void removeProduct(Product obj) {
+		products.remove(obj);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
