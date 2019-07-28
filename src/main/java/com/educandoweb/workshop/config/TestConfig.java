fdce4f6e10235764052c1874e595eab80448a8ed
Nelio Alves <acenelio@gmail.com>
2019-07-28 06:21:37 -0300

    Database seeding

---
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.educandoweb.workshop.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.workshop.entities.Category;
+import com.educandoweb.workshop.repositories.CategoryRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+	}
+}
