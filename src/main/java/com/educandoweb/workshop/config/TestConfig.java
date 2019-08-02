36b9eb8598438660bd29c25722659320f92d7fa5
Nelio Alves <acenelio@gmail.com>
2019-08-01 23:22:48 -0300

    OrderItem instantiation

--- src/main/java/com/educandoweb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -10,11 +10,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.workshop.entities.Category;
 import com.educandoweb.workshop.entities.Order;
+import com.educandoweb.workshop.entities.OrderItem;
 import com.educandoweb.workshop.entities.Payment;
 import com.educandoweb.workshop.entities.Product;
 import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.entities.enums.OrderStatus;
 import com.educandoweb.workshop.repositories.CategoryRepository;
+import com.educandoweb.workshop.repositories.OrderItemRepository;
 import com.educandoweb.workshop.repositories.OrderRepository;
 import com.educandoweb.workshop.repositories.PaymentRepository;
 import com.educandoweb.workshop.repositories.ProductRepository;
@@ -38,6 +40,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private PaymentRepository paymentRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -71,11 +76,24 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p4, 1, p4.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		o1.getItems().add(oi1);
+		o1.getItems().add(oi2);
+		
+		o2.getItems().add(oi3);
+		
+		o3.getItems().add(oi4);
+		
 		Payment pay1 = new Payment(null, Instant.parse("2019-06-23T13:02:55Z"), o1);
 		o1.setPayment(pay1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		paymentRepository.saveAll(Arrays.asList(pay1));
 	}
 }
