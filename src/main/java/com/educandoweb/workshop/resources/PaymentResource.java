de8d0bc6a309f55f8008ef5221e5da61e6d4fc57
Nelio Alves <acenelio@gmail.com>
2019-08-15 17:36:34 -0300

    Fix - Payment retrievable

--- src/main/java/com/educandoweb/workshop/resources/PaymentResource.java
+++ src/main/java/com/educandoweb/workshop/resources/PaymentResource.java
@@ -8,12 +8,13 @@ import com.educandoweb.workshop.dto.PaymentDTO;
 import com.educandoweb.workshop.entities.Payment;
 import com.educandoweb.workshop.resources.generics.CreateResource;
 import com.educandoweb.workshop.resources.generics.RetrieveResource;
+import com.educandoweb.workshop.resources.generics.UpdateResource;
 import com.educandoweb.workshop.services.PaymentService;
 import com.educandoweb.workshop.services.generics.CRUDService;
 
 @RestController
 @RequestMapping(value="/payments")
-public class PaymentResource implements CreateResource<Payment, PaymentDTO, Long>, RetrieveResource<Payment, PaymentDTO, Long> {
+public class PaymentResource implements CreateResource<Payment, PaymentDTO, Long>, RetrieveResource<Payment, PaymentDTO, Long>, UpdateResource<Payment, PaymentDTO, Long> {
 
 	@Autowired
 	private PaymentService service;
